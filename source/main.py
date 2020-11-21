83465f6ebebc87c4abcf87cf5efc9176551c2baa
Javier Portillo Medina <javierportillo93@gmail.com>
2020-11-21 21:02:41 +0100

    Clean code

--- source/main.py
+++ source/main.py
@@ -1,100 +1,93 @@
-import requests
-from requests.exceptions import ProxyError
+import re
 import json
-from bs4 import BeautifulSoup
 import random
 import logging
-import re
+import requests
 
-GITHUB_SEARCH_URL = "https://github.com/search?q={keywords}&type={type}"
-GITHUB_OWNER_REGEX = r"https://github.com/(.+)/.*"
+from bs4 import BeautifulSoup
+from requests.exceptions import ProxyError, Timeout
 
 
 def get_config():
-    config = {
-        "keywords": [
-            "openstack",
-            "nova",
-            "css"
-        ],
-        "proxies": [
-            "51.75.147.43:3128",
-            "51.75.147.41:3128",
-            "136.243.254.196:80",
-            "62.23.15.92:3128"
-        ],
-        "type": "Repositories"
-    }
+    with open('config.yml', 'r') as file:
+        config = json.load(file)
     return config
 
 
-def get_html(config):
-    keywords = "+".join(config["keywords"])
-    proxy_list = config["proxies"].copy()
-    random.shuffle(proxy_list)
-    search_url = GITHUB_SEARCH_URL.format(keywords=keywords, type=config["type"])
-    response = call_url_with_proxy(search_url, proxy_list)
-    return response.text
+class GithubCrawler:
 
+    GITHUB_SEARCH_URL = "https://github.com/search?q={keywords}&type={type}"
+    GITHUB_OWNER_REGEX = r"https://github.com/(.+)/.*"
 
-def call_url_with_proxy(url, proxy_list):
-    while True:
-        try:
-            rand_proxy = proxy_list.pop()
-            proxies = {
-                "http": rand_proxy,
-                "https": rand_proxy
-            }
-            r = requests.get(url, proxies=proxies)
-        except ProxyError as e:
-            logging.warning(e)
-            continue
-        except IndexError:
-            raise ProxyError("No proxy is available")
-        break
-    return r
+    def __init__(self, keywords, proxies, type):
+        self.keywords = keywords
+        self.proxies = proxies
+        self.type = type
 
+    def _call_url_with_proxy(self, url):
+        random.shuffle(self.proxies)
+        proxy_list = self.proxies.copy()
+        while True:
+            try:
+                rand_proxy = proxy_list.pop()
+                proxies = {
+                    "http": rand_proxy,
+                    "https": rand_proxy
+                }
+                r = requests.get(url, proxies=proxies, timeout=5)
+                r.raise_for_status()
+            except (ProxyError, Timeout) as e:
+                logging.warning(e)
+                continue
+            except IndexError:
+                raise ProxyError("No proxy is available")
+            break
+        return r
 
-def get_user_from_url(url):
-    m = re.search(GITHUB_OWNER_REGEX, url)
-    return m.group(1)
+    def get_search_html(self):
+        keywords = "+".join(self.keywords)
+        search_url = self.GITHUB_SEARCH_URL.format(keywords=keywords, type=self.type)
+        response = self._call_url_with_proxy(search_url)
+        return response.text
 
+    def parse_search_html(self, html_str):
+        git_html = BeautifulSoup(html_str, "html.parser")
+        repo_list_html = git_html.find_all("li", attrs={"class": "repo-list-item"})
+        results = []
+        for repo_item_html in repo_list_html:
+            json_data = json.loads(repo_item_html.a["data-hydro-click"])
+            repo_url = json_data["payload"]["result"]["url"]
+            response_json = {"url": repo_url}
+            results.append(response_json)
+        return results
 
-def get_language_stats_from_url(url, config):
-    proxy_list = config["proxies"].copy()
-    random.shuffle(proxy_list)
-    response = call_url_with_proxy(url, proxy_list)
-    git_html = BeautifulSoup(response.text, "html.parser")
-    languages_list = git_html.find_all("span", attrs={"class": "Progress-item",
-                                                      "itemprop": "keywords"})
-    lang_dict = {}
-    for lang in languages_list:
-        attr_split = lang["aria-label"].split()
-        lang_dict[attr_split[0]] = float(attr_split[1])
-    return lang_dict
+    def get_user_from_url(self, url):
+        m = re.search(self.GITHUB_OWNER_REGEX, url)
+        return m.group(1)
 
+    def get_extra_from_url(self, url):
+        response = self._call_url_with_proxy(url)
+        return {"owner": self.get_user_from_url(url),
+                "language_stats": self.parse_language_html(response.text)}
 
-def parse_html(html_str):
-    git_html = BeautifulSoup(html_str, "html.parser")
-    repo_list_html = git_html.find_all("li", attrs={"class": "repo-list-item"})
-
-    results = []
-    for repo_item_html in repo_list_html:
-        json_data = json.loads(repo_item_html.a["data-hydro-click"])
-        repo_url = json_data["payload"]["result"]["url"]
-        repo_user = get_user_from_url(repo_url)
-        response_json = {"url": repo_url,
-                         "extra": {"owner": repo_user}}
-        results.append(response_json)
-    return results
+    def parse_language_html(self, html_str):
+        git_html = BeautifulSoup(html_str, "html.parser")
+        languages_list = git_html.find_all("span", attrs={"class": "Progress-item",
+                                                          "itemprop": "keywords"})
+        lang_dict = {}
+        for lang in languages_list:
+            attr_split = lang["aria-label"].split()
+            lang_dict[attr_split[0]] = float(attr_split[1])
+        return lang_dict
 
 
 def main():
     config = get_config()
-    response_html = get_html(config)
-    json_response = parse_html(response_html)
+    git_crw = GithubCrawler(**config)
+    search_html = git_crw.get_search_html()
+    json_response = git_crw.parse_search_html(search_html)
     for item in json_response:
-        item["extra"]["language_stats"] = get_language_stats_from_url(item["url"], config)
+        item["extra"] = git_crw.get_extra_from_url(item["url"])
     print(json.dumps(json_response))
 
 
